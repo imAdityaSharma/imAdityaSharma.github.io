955d14e44ad88dffc73578b510eedf2d32857e5c
aditya m sharma <adityambha1999@gmail.com>
2024-02-09 00:32:50 +0530

    animate background cat done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-  var images = ["cat1.jpg", "cat2.jpg"]; // Array of image URLs
-  var index = 0; // Index to keep track of the current image
+// document.addEventListener('DOMContentLoaded', function() {
+//   var images = ["cat1.jpg", "cat2.jpg"]; // Array of image URLs
+//   var index = 0; // Index to keep track of the current image
 
-  function changeBackgroundImage() {
-    // Change the background image
-    document.body.style.backgroundImage = "url('" + images[index] + "')";
-    document.body.style.backgroundSize="cover"
+//   function changeBackgroundImage() {
+//     // Change the background image
+//     document.body.style.backgroundImage = "url('" + images[index] + "')";
+//     document.body.style.backgroundSize="cover"
 
-    // Increment index to switch to the next image
-    index = (index + 1) % images.length;
-  }
+//     // Increment index to switch to the next image
+//     index = (index + 1) % images.length;
+//   }
 
-  // Call the function every 10 seconds
-  setInterval(changeBackgroundImage, 10000);
-});
+//   // Call the function every 10 seconds
+//   setInterval(changeBackgroundImage, 10000);
+// });
